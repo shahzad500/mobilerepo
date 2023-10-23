656d9ffe1640b1eac96cd967139be1e22f98a69e
shahzad500 <shahzadanjum47619@gmail.com>
2023-10-23 14:55:12 +0530

    Initial commit

---
+++ src/main/java/com/cg/entity/Mobile.java
@@ -0,0 +1,40 @@
+package com.cg.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Mobile {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	
+	public Mobile() {}
+	public Mobile(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Mobile [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+
+}
